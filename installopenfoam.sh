8434df202101e2387825e38b67deec27dd561389
mwilliams2112 <78158890+mwilliams2112@users.noreply.github.com>
2022-10-09 16:38:37 +0100

    Create installopenfoam.sh

---
+++ installopenfoam.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo add-apt-repository http://dl.openfoam.org/ubuntu
+sudo sh -c "wget -O - http://dl.openfoam.org/gpg.key | apt-key add -"
+
+sudo apt-get update
+sudo apt-get -y install openfoam240
+sudo apt-get -y install paraviewopenfoam410
+
+echo 'source /opt/openfoam240/etc/bashrc' >> .bashrc
+
+mkdir -p $FOAM_RUN
+cd $FOAM_RUN
+cp -r $FOAM_TUTORIALS .
+cd
+
+
